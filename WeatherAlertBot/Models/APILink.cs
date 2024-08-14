104340cd8a0031f8573c1a45a4b58f8379da734c
семенец Иван <ivan.g.sem@gmail.com>
2024-08-14 06:34:08 +0300

    Add several models

---
+++ WeatherAlertBot/Models/APILink.cs
@@ -0,0 +1,26 @@
+﻿using GoogleMaps.LocationServices;
+
+namespace WeatherAlertBot.Models
+{
+    public class APILink
+    {
+        private double Latitude {  get; set; }
+        private double Longitude { get; set; }
+
+        public string UpdatedAPILink(UserSettings settings)
+        {
+            string link = $"https://api.open-meteo.com/v1/forecast?latitude={Latitude}&longitude={Longitude}&current=temperature_2m,rain,wind_speed_10m&forecast_days=1";
+            return link;
+        }
+        private void LocationConvertor(UserSettings settings)
+        {
+            var address = settings.Location;
+
+            var locationService = new GoogleLocationService();
+            var point = locationService.GetLatLongFromAddress(address);
+
+            Latitude = point.Latitude;
+            Longitude = point.Longitude;
+        }
+    }
+}
---
+++ WeatherAlertBot/Models/UserSettings.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace WeatherAlertBot.Models
+{
+    public class UserSettings
+    {
+        public long ChatID { get; set; }
+        public string Location { get; set; }
+        public string UpdateInterval { get; set; }
+        public TimeOnly MorningTime { get; set; }
+    }
+}
