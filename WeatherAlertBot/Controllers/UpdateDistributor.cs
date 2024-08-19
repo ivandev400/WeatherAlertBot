83130a519402df17c045649c74be1c8aa1049d49
семенец Иван <ivan.g.sem@gmail.com>
2024-08-19 18:59:21 +0300

    Create Distributor class

---
+++ WeatherAlertBot/Controllers/UpdateDistributor.cs
@@ -0,0 +1,29 @@
+﻿using Telegram.Bot.Types;
+using WeatherAlertBot.Interfaces;
+
+namespace WeatherAlertBot.Controllers
+{
+    public class UpdateDistributor<T> where T : ITelegramUpdateListener, new()
+    {
+        private Dictionary<long, T> listeners;
+
+        public UpdateDistributor()
+        {
+            listeners = new Dictionary<long, T>();
+        }
+
+        public async Task GetUpdate(Update update)
+        {
+            long chatId = update.Message.Chat.Id;
+            T? listener = listeners.GetValueOrDefault(chatId);
+            if(listener == null)
+            {
+                listener = new T();
+                listeners.Add(chatId, listener);
+                await listener.GetUpdate(update);
+                return;
+            }
+            await listener.GetUpdate(update);
+        }
+    }
+}
---
+++ WeatherAlertBot/Interfaces/ITelegramUpdateListener.cs
@@ -0,0 +1,9 @@
+﻿using Telegram.Bot.Types;
+
+namespace WeatherAlertBot.Interfaces
+{
+    public interface ITelegramUpdateListener
+    {
+        Task GetUpdate(Update update);
+    }
+}
