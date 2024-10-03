e9eb9a6aa2b79343458994b73cb8d3ce856c212c
семенец Иван <ivan.g.sem@gmail.com>
2024-10-03 06:29:28 +0300

    Now the user can get the graph of daily weather hourly predicted , well

--- WeatherAlertBot/Controllers/Commands/DailyWeatherCommand.cs
+++ WeatherAlertBot/Controllers/Commands/DailyWeatherCommand.cs
@@ -5,6 +5,9 @@ using WeatherAlertBot.Models;
 using WeatherAlertBot.Services;
 using ScottPlot;
 using ScottPlot.Colormaps;
+using ScottPlot.AxisLimitManagers;
+using System.Reflection.Emit;
+using ScottPlot.Plottables;
 
 namespace WeatherAlertBot.Controllers.Commands
 {
@@ -52,6 +55,7 @@ namespace WeatherAlertBot.Controllers.Commands
                 $"Average wind speed: {Math.Round(morningData.Average(x => x.windSpeed), 1)}km/h";
 
             await Client.SendTextMessageAsync(chatId, textMessage, replyMarkup: replyMarkup.GetPermanentMarkup(userSettings.Language));
+            await GenerateWeatherPlot(hourlyWeather, update, userSettings.Language);
             Recommendation = null;
         }
 
@@ -70,6 +74,53 @@ namespace WeatherAlertBot.Controllers.Commands
             ));
 
             return filteredData;
+        }
+
+        public async Task GenerateWeatherPlot(HourlyWeather hourlyWeather, Update update, string language)
+        {
+            var plot = new Plot();
+
+            List<int> dataX = hourlyWeather.Hours.Select(h => h.Hour).ToList();
+            List<double> temperatureDataY = hourlyWeather.Temperature;
+            List<double> windSpeedDataY = hourlyWeather.WindSpeed;
+
+            double[] doubleX = dataX.Select(x => (double)x).ToArray();
+
+            var windSpeedScatter = plot.Add.Scatter(dataX, windSpeedDataY, color: ScottPlot.Color.FromColor(System.Drawing.Color.Green));
+            var tempScatter = plot.Add.Scatter(dataX, temperatureDataY, color: ScottPlot.Color.FromColor(System.Drawing.Color.Red));
+
+            for (int i = 0; i < hourlyWeather.Rain.Count; i++)
+            {
+                if (hourlyWeather.Rain[i] > 0)
+                {
+                    var bar = new Bar();
+                    bar.Value = hourlyWeather.Rain[i];
+                    bar.Position = dataX[i];
+                    bar.FillColor = ScottPlot.Color.FromColor(System.Drawing.Color.DarkBlue);
+                    bar.BorderLineWidth = 0;  
+                    
+                    if (i == 0) { bar.Label = language == "en" ? "Rain mm" : "Опади mm"; }
+
+                    plot.Add.Bar(bar);
+                }
+            }
+
+            tempScatter.LegendText = language == "en" ? "Temperature °C" : "Температура °C";
+            windSpeedScatter.LegendText = language == "en" ? "Wind Speed km/h" : "Швидкість вітру km/h";
+
+            plot.Title(language == "en" ? "Daily weather" : "Погода на день");
+            plot.XLabel(language == "en" ? "Hours" : "Години");
+            plot.YLabel(language == "en" ? "Values" : "Значення");
+
+            var filePath = "demo.png";
+            plot.SavePng(filePath, 800, 400);
+
+            using (var stream = System.IO.File.OpenRead(filePath))
+            {
+                await Client.SendPhotoAsync(update.Message.Chat.Id, new InputFileStream(stream));
+
+                System.IO.File.Delete(filePath);
+            }
         }
     }
 }
